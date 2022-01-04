34a20a981b4381e78cb6d6759acbd72aa5903784
who-man-tech <enes515254@gmail.com>
2022-01-04 08:26:45 +0200

    Update settings

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from settings.common import ALLOW_ORIGINS, ALLOW_HEADERS
+
 from models.users import User
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=ALLOW_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=ALLOW_HEADERS,
 )
 
 
--- settings/common.py
+++ settings/common.py
@@ -1,3 +1,6 @@
 import os
 
+
+ALLOW_ORIGINS = ['*']
+ALLOW_HEADERS = ['*']
 DATABASE_URL = os.getenv("DATABASE_URL", 'postgresql+asyncpg://main:main@localhost:5432/main')
