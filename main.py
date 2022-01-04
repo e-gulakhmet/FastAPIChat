fc5b66214b16092dba76eb292e4b73bc331a5343
who-man-tech <enes515254@gmail.com>
2022-01-04 07:06:40 +0200

    Add migrate command to manage.py and add cors to main

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from models.users import User
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/ping")
--- manage.py
+++ manage.py
@@ -2,29 +2,43 @@ import argparse
 import os
 
 
+class Commands:
+    @staticmethod
+    def run(args: argparse.Namespace):
+        port = args.port
+        host = args.host
+        os.system(f'sudo uvicorn main:app --reload --workers 1 --host {host} --port {port}')
+
+    @staticmethod
+    def make_migrations(args: argparse.Namespace):
+        message = args.message
+        assert message, 'Set migration message arg(check help)'
+        os.system(f'alembic revision --autogenerate -m "{message}"')
+
+    @staticmethod
+    def migrate():
+        os.system('alembic upgrade head')
+
+
 def _init_parser():
     parser = argparse.ArgumentParser(prog='FastAPIChat', description='App management')
 
-    parser.add_argument('command', help='Command to execute')
+    parser.add_argument('command', help='Commands to execute: run, migrate, make_migrations')
 
+    # run command args
     parser.add_argument('--port', action='store', type=int, default=80, help='Server post')
     parser.add_argument('--host', action='store', type=str, default='0.0.0.0', help='Server host')
+    # make_migrations command args
+    parser.add_argument('--message', action='store', type=str, help='Migration message')
 
     return parser
-
-
-def run(args: argparse.Namespace):
-    port = args.port
-    host = args.host
-    os.system('pwd')
-    os.system(f'sudo uvicorn main:app --reload --workers 1 --host {host} --port {port}')
 
 
 def main():
     parser = _init_parser()
     args = parser.parse_args()
 
-    globals()[args.command](args)
+    getattr(Commands, args.command)(args)
 
 
 if __name__ == "__main__":
