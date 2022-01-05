9ae205f7c5ac1f977dcb5534ced9dbb37ee51752
who-man-tech <enes515254@gmail.com>
2022-01-05 11:04:27 +0200

    Add settings configs for test, stage, prod and local

--- settings/common.py
+++ settings/common.py
@@ -1,6 +1,15 @@
 import os
 
-
+# Basic
 ALLOW_ORIGINS = ['*']
 ALLOW_HEADERS = ['*']
+
+# DATABASE
 DATABASE_URL = os.getenv("DATABASE_URL", 'postgresql+asyncpg://main:main@localhost:5432/main')
+
+# JWT
+JWT_SECRET = os.getenv('JWT_SECRET', 'fake_secret')
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', '30')
+ALGORITHM = ''
+
+
---
+++ settings/core.py
@@ -0,0 +1,5 @@
+import os
+
+SETTINGS_PATH = os.getenv('SETTINGS_PATH', 'settings.common')
+
+from SETTINGS_PATH import *
