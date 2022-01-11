44c63e96fa90b3d601764fd72d500427850f38d0
who-man-tech <enes515254@gmail.com>
2022-01-11 08:39:31 +0200

    Add users tortoise models

--- users/models.py
+++ users/models.py
@@ -1,12 +1,54 @@
-from sqlmodel import SQLModel, Field
+from typing import Optional
+
+from tortoise import fields
+from tortoise.exceptions import DoesNotExist
+
+from core.models import BaseDBModel
+from users.schemes import UserCreateScheme
+from users.services import UserService
 
 
-class User(SQLModel, table=True):
-    id: int = Field(primary_key=True, index=True)
-    hashed_password: str = Field(nullable=False)
-    first_name: str = Field(nullable=True, default=None)
-    last_name: str = Field(nullable=True, default=None)
-    username: str = Field(nullable=True, default=None, min_length=3, max_length=100)
-    email: str = Field(nullable=False, min_length=3, max_length=100)  # TODO: Добавить проверку почты по регулярке
+class User(BaseDBModel):
+    username = fields.CharField(max_length=20, unique=True)
+    email = fields.CharField(max_length=255, unique=True)
+    first_name = fields.CharField(max_length=50, null=True)
+    last_name = fields.CharField(max_length=50, null=True)
+    password_hash = fields.CharField(max_length=128, null=True)
+    is_superuser = fields.BooleanField(default=False)
 
+    def full_name(self) -> str:
+        if self.first_name or self.last_name:
+            return f"{self.first_name or ''} {self.last_name or ''}".strip()
+        return self.username
 
+    @classmethod
+    async def get_by_email(cls, email: str) -> Optional["User"]:
+        try:
+            query = cls.get_or_none(email=email)
+            user = await query
+            return user
+        except DoesNotExist:
+            return None
+
+    @classmethod
+    async def get_by_username(cls, username: str) -> Optional["User"]:
+        try:
+            query = cls.get(username=username)
+            user = await query
+            return user
+        except DoesNotExist:
+            return None
+
+    @classmethod
+    async def create(cls, user_obj: UserCreateScheme) -> "User":
+        user_dict = user_obj.dict()
+        password_hash = UserService.hash_password(user_obj.password)
+        user = cls(**user_dict, password_hash=password_hash)
+        await user.save()
+        return user
+
+    class Meta:
+        table = 'users'
+
+    class PydanticMeta:
+        computed = ["full_name"]
--- users/schemes.py
+++ users/schemes.py
@@ -1,27 +1,40 @@
 from typing import Optional
 
-from sqlmodel import Field, SQLModel
+from pydantic import BaseModel, EmailStr
 
 
-class UserCreateSchema(SQLModel):
-    email: str
+class BaseUserScheme(BaseModel):
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    username: Optional[str] = None
+    is_superuser: Optional[bool] = False
+
+
+class UserDBScheme(BaseUserScheme):
+    id: int
+    password_hash: str
+
+    class Config:
+        orm_mode = True
+
+
+class BaseUserOutScheme(BaseUserScheme):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreateScheme(BaseModel):
+    first_name: Optional[str]
+    last_name: Optional[str]
+    email: EmailStr
+    username: Optional[str] = None
     password: str
 
 
-class UserUpdateSchema(SQLModel):
-    first_name: Optional[str]
-    last_name: Optional[str]
-    username: str = Field(nullable=False, min_length=3, max_length=100)
-
-
-class UserGetSchema(SQLModel):
-    id: int
+class UserUpdateScheme(BaseModel):
     first_name: Optional[str]
     last_name: Optional[str]
     username: Optional[str]
-    email: str
-
-
-class UserLoginSchema(SQLModel):
-    email: str
-    password: str
